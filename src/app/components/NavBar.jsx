b3207a4a78ec0ad702b62ae4de4cd05786b761fa
hikmalafandi <ihkmalafandi20@gmail.com>
2025-08-28 14:18:34 +0700

    change navbar file

--- src/app/components/NavBar.jsx
+++ src/app/components/NavBar.jsx
@@ -4,7 +4,7 @@ export default function Navbar() {
 
   return (
     <nav className="bg-black text-white px-4 py-3 shadow-md">
-      <div className="container mx-auto flex items-center justify-between md:justify-center">
+      <div className="container mx-auto flex items-center justify-center">
         {/* Logo */}
         <div className="text-xl font-bold">SHOP.CO</div>
       </div>
